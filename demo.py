cb1a621e90ff6186711fcb10cd07acc4ca9e4c50
syh <syh20131292@gmail.com>
2024-06-05 18:02:55 +0900

    SAM Test

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 import numpy as np
 import pandas as pd
 import cv2
-from segment_anything import SamPredictor, sam_model_registry
+from segment_anything import SamPredictor, sam_model_registry, SamAutomaticMaskGenerator
 import coord_transform as ct
 import torch
 import matplotlib.pyplot as plt
@@ -18,7 +18,20 @@ def show_mask(mask, ax, random_color=False):
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
+def show_anns(anns,ax):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    # ax = plt.gca()
+    # ax.set_autoscale_on(False)
 
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.55]])
+        img[m] = color_mask
+    ax.imshow(img)
 def show_points(coords, labels, ax, marker_size=375):
     pos_points = coords[labels == 1]
     neg_points = coords[labels == 0]
@@ -55,6 +68,8 @@ class ImageClassifierApp:
         self.fail_button = tk.Button(self.root, text="Fail", command=self.mark_fail)
         self.fail_button.pack(side=tk.RIGHT)
 
+        self.canvas = None
+
         self.roi_detector = roi_detector
         self.show_image()
         self.root.mainloop()
@@ -71,15 +86,16 @@ class ImageClassifierApp:
         depth = Image.open(depth_path)
         depth = np.array(depth)
 
-        output = self.roi_detector.set_image(image)
-
+        # output = self.roi_detector.set_image(image)
+        masks= self.roi_detector.generate(image)
         # Get all segmentations for the entire image
-        masks, _, _ = self.roi_detector.predict(point_coords=None, point_labels=0)
+        # masks, _, _ = self.roi_detector.predict(point_coords=None, point_labels=None)
 
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
         ax.imshow(image)
-        for mask in masks:
-            show_mask(mask, ax)
+        # for mask in masks:
+        #     show_mask(mask, ax, random_color=True)
+        show_anns(masks,ax)
 
         plt.axis('off')
         fig.canvas.draw()
@@ -88,13 +104,15 @@ class ImageClassifierApp:
 
     def show_image(self):
         if self.current_index < len(self.image_files):
+            if self.canvas is not None:
+                self.canvas.get_tk_widget().destroy()
+
             image_num = self.image_files[self.current_index].split('_')[0]
             fig, ax = self.update_images(image_num)
 
-            canvas = FigureCanvasTkAgg(fig, master=self.left_frame)
-            canvas.draw()
-            canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
+            self.canvas = FigureCanvasTkAgg(fig, master=self.left_frame)
+            self.canvas.draw()
+            self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         else:
             tk.Label(self.left_frame, text="No more images.").pack()
 
@@ -120,10 +138,20 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     sam = sam_model_registry["default"](checkpoint="/media/syh/ssd2/data/sam_checkpoint/sam_vit_h_4b8939.pth")
     sam.to(device)
-    predictor = SamPredictor(sam)
+    # mask_generator = SamAutomaticMaskGenerator(sam)
+    mask_generator_2 = SamAutomaticMaskGenerator(
+        model=sam,
+        points_per_side=32,
+        pred_iou_thresh=0.86,
+        stability_score_thresh=0.92,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,  # Requires open-cv to run post-processing
+    )
+    # predictor = SamPredictor(sam)
 
     dir_in = "/media/syh/ssd2/data/order_240523/20240523_trim"
     csv_okay = "okay_dummy_.csv"
     csv_fail = "fail_dummy_.csv"
 
-    app = ImageClassifierApp(predictor, dir_in, csv_okay, csv_fail)
+    app = ImageClassifierApp(mask_generator_2, dir_in, csv_okay, csv_fail)
